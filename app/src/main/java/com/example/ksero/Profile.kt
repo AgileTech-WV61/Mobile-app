445aa0f29e00f6d8d915b823a84ac157c0aa1df7
kevin <67239585+kevinstif@users.noreply.github.com>
2022-09-21 16:20:54 -0500

    add screen of profile user

--- app/src/main/java/com/example/ksero/Profile.kt
+++ app/src/main/java/com/example/ksero/Profile.kt
@@ -1,13 +1,11 @@
 package com.example.ksero
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_profile)
     }
 }
