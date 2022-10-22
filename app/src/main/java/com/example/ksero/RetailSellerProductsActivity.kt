2ca87f3ec9474966cec697828de6c15d5ce6d45c
Caleb <reateguicevallosc@gmail.com>
2022-10-22 12:29:45 -0500

    Add:  Function to add products to the shopping cart
    Retail-Seller P

--- app/src/main/java/com/example/ksero/RetailSellerProductsActivity.kt
+++ app/src/main/java/com/example/ksero/RetailSellerProductsActivity.kt
@@ -1,16 +1,43 @@
 package com.example.ksero
 
-import Beans.RProducts
+import Beans.CartOrder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.ksero.retailSeller_products.AdapterRProducts
-    
+
 class RetailSellerProductsActivity : AppCompatActivity() {
+
+    private lateinit var adapter: AdapterRetailSellerCart
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var item: ArrayList<CartOrder>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_retail_seller_products)
+
+        val btnAdd:Button = findViewById(R.id.btnAddToCart)
+
+        //Añadir al Carrito de Compras
+        btnAdd.setOnClickListener(){
+            val productName:TextView = findViewById(R.id.textViewItemNameValue)
+            val productPrice:TextView = findViewById(R.id.textViewItemTotalPriceValue)
+            val productQuantity:TextView = findViewById(R.id.textViewItemQuantityValue)
+
+            item.add(CartOrder(productName.text.toString(),
+                productPrice.text.toString().toDouble(),
+                productQuantity.text.toString().toInt()))
+
+            val layoutManager = LinearLayoutManager(this)
+            recyclerView = findViewById(R.id.recyclerViewCart)
+            recyclerView.layoutManager = layoutManager
+            recyclerView.setHasFixedSize(true)
+            adapter = AdapterRetailSellerCart(item)
+            recyclerView.adapter = adapter
+        }
+
         getAllProducts()
     }
 
